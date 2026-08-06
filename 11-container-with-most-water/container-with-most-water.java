fbda18078b6fc5c5a11a79078da2c79f5686742e
samyak gaure <samyakg051@gmail.com>
2026-08-06 22:22:36 +0530

    Time: 2 ms (99.62%) | Memory: 77.4 MB (29.77%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,17 +1,34 @@
 class Solution {
     public int maxArea(int[] height) {
-        int n = height.length;
-        int i = 0;
-        int j = n-1;
-        int area = 0;
-        while(i<j){
-            int wide = Math.min(height[i],height[j]);
-            area = Math.max(area,wide*(j-i));
-            if(height[i]<height[j]){
-                i++;
-            }else{
-                j--;
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentWidth = right - left;
+            int hLeft = height[left];
+            int hRight = height[right];
+
+            // Calculate area based on the shorter line
+            if (hLeft < hRight) {
+                int area = hLeft * currentWidth;
+                if (area > maxArea) maxArea = area;
+                
+                // Fast-forward the left pointer past any lines that are shorter 
+                // than the one we just evaluated, as they cannot possibly yield a larger area.
+                while (left < right && height[left] <= hLeft) {
+                    left++;
+                }
+            } else {
+                int area = hRight * currentWidth;
+                if (area > maxArea) maxArea = area;
+                
+                // Fast-forward the right pointer past any shorter lines.
+                while (left < right && height[right] <= hRight) {
+                    right--;
+                }
             }
-        } return area;
+        }
+        return maxArea;
     }
 }
